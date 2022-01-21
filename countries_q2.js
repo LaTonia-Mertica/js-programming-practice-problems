bd63f0b6748ce19c551f7082850832107c786727
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-21 16:01:46 -0500

    countries question 2 completed

--- countries_q2.js
+++ countries_q2.js
@@ -6,12 +6,21 @@ const readlines = () => {
   let begin_end_vowel = [];
   let vowels = ["A", "E", "I", "O", "U", "Y"];
 
-  for (const word of countriesByLine) {
-    if (word.charAt(0) === "A") {
-      begin_end_vowel.push(word);
-    }
-  }
+  for (const country of countriesByLine) {
+    let starting_letter = country.toUpperCase().charAt(0);
+    let ending_letter = country.toUpperCase().charAt(country.length - 1);
 
+    if (
+      vowels.indexOf(starting_letter) > -1 &&
+      vowels.indexOf(ending_letter) > -1
+    ) {
+      begin_end_vowel.push(country);
+    }
+    console.log(country);
+    console.log(starting_letter);
+    console.log(ending_letter);
+    // console.log(vowels.indexOf(country.charAt(0)));
+  }
   console.log(begin_end_vowel);
 };
 
