8cc30dfbf46b4741c2c33d65524545cf2a29cb9e
La'Tonia Mertica <latoniamertica@gmail.com>
2022-05-27 01:08:25 -0400

    js practice: replace for loop with objects dot keys - nogo/code functional

--- letterToLetterCount.js
+++ letterToLetterCount.js
@@ -20,18 +20,13 @@ THE GOAL IS TO:
 note: this is practice for solving the first part of scrabble solver .. . thanks danny🤓
 */
 
+// can we refactor our for loop to use object.keys instead
+
 function getLetterToLetterCount(word) {
-  if (typeof word !== "string") {
-    return "Please Enter a Valid Value!";
-  }
   const wordObj = {};
-  const wordArr = word.split("");
-  for (let i = 0; i < wordArr.length; i++) {
-    const letter = wordArr[i].toLowerCase();
-    const invalidChar = [" ", "'"];
-    if (invalidChar.includes(letter)) {
-      continue;
-    }
+  let letterArr = word.split("");
+  for (let i = 0; i < letterArr.length; i++) {
+    const letter = letterArr[i];
     if (wordObj[letter] === undefined) {
       wordObj[letter] = 1;
     } else {
@@ -41,3 +36,23 @@ function getLetterToLetterCount(word) {
   return wordObj;
 }
 console.log(getLetterToLetterCount("apple"));
+
+/*
+THE GOAL IS TO:
+- fn that accepts two letter objects
+- in fn there must be enough random letters to create sought word  ... this requires a count of valid letters available to create the word
+- fn returns a boolean that states true or false that can make the word with available letters
+*/
+const scrabbleLetters = getLetterToLetterCount("awepples");
+const validWord = getLetterToLetterCount("apples");
+function canWordBeCreated(scrabbleLetters, validWord) {
+  let canCreateWord = true;
+  for (const [key, value] of Object.entries(validWord)) {
+    if (scrabbleLetters[key] === undefined || scrabbleLetters[key] < value) {
+      canCreateWord = false;
+      break;
+    }
+  }
+  return canCreateWord;
+}
+console.log(canWordBeCreated(scrabbleLetters, validWord));
