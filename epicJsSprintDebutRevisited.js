995291625e64004470cce6babf37c2151f4123ce
La'Tonia Mertica <latoniamertica@gmail.com>
2022-05-28 22:32:41 -0400

    js practice: part 2 / epic js sprint debut

---
+++ epicJsSprintDebutRevisited.js
@@ -0,0 +1,67 @@
+/*
+BABY NAMES
+        [] What is the shortest baby name in the top 40 baby names 
+           for 2020 - and handle ties?
+        [] What are the longest baby names in the top 40 baby 
+           names for 2020 (handle ties)? 
+        [] Find baby names in 2020 that when spelled backwards are 
+           valid scrabble words.
+            [] Solve this two ways: 
+                [] with an array to hold the Scrabble words
+                [] with a dictionary/set to hold the Scrabble words
+                [] time both implementations
+        [] What are all of the names that were top 40 baby names  
+           in both 1880 and 2020?
+
+COUNTRIES
+        [] What countries have “United” in the name?
+        [] What countries both begin and end with a vowel? 
+        [] What country names are > 50% vowels?
+        [] What is the shortest country name - and handle ties?
+        [] What countries use only one vowel in their name (vowel 
+           can be used multiple times)?
+        [] What country names contain another country name? 
+
+MISCELLANEOUS
+        [] What are all of the words that can only be made from 
+           the letters “R”, “S”, “T”, “L”, “N”, and “E”?
+
+TOP MOVIES
+        [] What movies on this list were distributed by 
+           DreamWorks? 
+        [] What is the highest grossing moving from Universal  
+           Pictures, domestically?
+        [] What distributor has the most films on this list?
+        [] What is the earliest year on this list, and what were 
+           the films from that year?
+        [] What is the distribution of ratings? (How many are PG, 
+           PG-13, R, etc.?) 
+
+WORDPLAY
+		 [] What are all of the words containing UU?
+		 [] What are all of the words containing an X and a Y and  
+            a Z? ... order irrelevant
+		 [] What are all of the words containing a Q but not a U?
+		 [] What are all of the words that contain the word CAT 
+            and are exactly 5 letters long?
+		 [] What are all of the words that have no E or A and are 
+            at least 15 letters long?
+		 [] What are all of the words that have a B and an X and 
+            are less than 5 letters long?
+		 [] What are all of the words that start and end with a Y?
+		 [] What are all of the words with no vowel and not even a 
+            Y?
+		 [] What are all of the words that have all 5 vowels, in 
+            any order?
+		 [] What are all of the words that have all 5 vowels, in 
+            alphabetical order? ... duplicates ok
+		 [] How many words contain the substring "TYPE”?
+		 [] Create and print an array containing all of the words 
+            that end in "GHTLY"
+		 [] What is the shortest word that contains all 5 vowels?
+		 [] What is the longest word that contains no vowels?
+		 [] Which of the letters Q, X, and Z is the least common?
+		 [] What is the longest palindrome?
+		 [] What are all of the letters that never appear 
+            consecutively in an English word?
+*/
