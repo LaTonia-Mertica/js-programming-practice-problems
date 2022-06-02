d91e2186b178613d7ac57e43389d4f4b1eda35d8
La'Tonia Mertica <latoniamertica@gmail.com>
2022-06-02 16:01:48 -0400

    danny: specified js practice

--- letterToLetterCount.js
+++ letterToLetterCount.js
@@ -56,3 +56,94 @@ function canWordBeCreated(scrabbleLetters, validWord) {
   return canCreateWord;
 }
 console.log(canWordBeCreated(scrabbleLetters, validWord));
+
+/*
+Write a function that takes a string availableLetters as an argument and returns an array of all of the words that can be made from only thoseletters. Letters can be re-used as many times as needed and can appear in any order. Not all of the letters in availableLetters have to be used. */
+const fs = require("fs");
+const wordStr = fs
+  .readFileSync("sowpods.txt")
+  .toString("utf-8")
+  .toUpperCase()
+  .trim();
+const wordArr = wordStr.split("\n");
+
+function getPossibleWords(availableLetters) {
+  const result = [];
+
+  const availableLettersObj = getLetterToLetterCount(availableLetters);
+
+  for (let i = 0; i < wordArr.length; i++) {
+    const word = wordArr[i];
+    const validWordObj = getLetterToLetterCount(word);
+    if (canWordBeCreated(availableLettersObj, validWordObj)) {
+      result.push(word);
+    }
+  }
+  return result;
+}
+console.log(getPossibleWords("AAH"));
+
+/*
+NEW HELPER FN
+- for loop through words in validWordObj
+- read each letter of each word
+- check if there is a score for the word
+- if not add the value of the first letter
+- thereafter add the value of each additional letter
+- output a tally for the word
+
+EXPECTED OUT
+[{ word: "AA", score: 2 }, { word: "AAH", score: 6 }]
+*/
+const scores = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
+function getWordScoreObj(arr) {
+  const scoreOutput = [];
+  let lettersPerWordObj = {};
+  let theScore = {};
+
+  const wordToScoreObj = getPossibleWords(arr);
+
+  for (const word of wordToScoreObj) {
+    lettersPerWordObj = getLetterToLetterCount(word);
+    for (const [key, value] in Object.entries(scores)) {
+      if (Object.keys(lettersPerWordObj) in scores) {
+        if (scoreOutput[theScore] === undefined) {
+          scoreOutput[theScore] = scores[key];
+        } else {
+          scoreOutput[theScore] = scores[key]++;
+        }
+        scoreOutput.push((theScore[word] = scores[key[value]]));
+      }
+    }
+  }
+  return scoreOutput;
+}
+console.log(getWordScoreObj("AAH"));
