84040bd65f981bb9a4a6a2d74e50ed780ace6aa7
La'Tonia Mertica <latoniamertica@gmail.com>
2022-06-03 22:28:18 -0400

    adam : excluded letters; longest words

--- longestWordMadeFromExclusionOfSpecificLetters.js
+++ longestWordMadeFromExclusionOfSpecificLetters.js
@@ -1,10 +1,17 @@
 /*
 QUESTION: What is the longest word that can be made without the letters in “AEIOSHRTN”?
 
-[x] Create function to return boolean for whether words contain letters to be excluded
-[x] Create another function to return boolean for words that meet letter-exclusion criteria that 
-   returns another boolean for whether the longest word is defined
+[x] Create function to return boolean for whether a word contains letters to be excluded 
+
+... can use this same fn for list of words containing excluded letters AND for list of words not containing excluded letters
+
+[x] Create function to find longest word containing excluded letters
+[x] Create function to find longest word not containing excluded ltters
 [x] Create program that uses both functions to get the longest word AND ties
+
+... created two for loops inside one function ... verbose but seems to output as expected 
+
+
   
 NOTES:
 0. Use the scrabble words list provided in the sowpods.txt file for this program
@@ -14,93 +21,83 @@ NOTES:
 */
 
 const fs = require("fs");
-const wordStr = fs
+const wordArr = fs
   .readFileSync("sowpods.txt")
   .toString("utf-8")
   .toUpperCase()
-  .trim();
-const wordArr = wordStr.split("\n");
+  .trim()
+  .split("\n");
 
-// fn one with main variable globally scoped
-const wordsThatDoNotIncludeExcludedLetters = [];
-function getWordsThatDoNotIncludeExcludedLetters(str) {
+function isWordThatContainsExcludedLetters(word) {
   const excludedLetters = ["A", "E", "I", "O", "S", "H", "R", "T", "N"];
 
-  for (let i = 0; i < wordArr.length; i++) {
-    const word = wordArr[i];
-    for (let j = 0; j < word.length; j++) {
-      const letter = word[j];
-
-      if (excludedLetters.includes(letter)) {
-        break;
-      } else if (j === word.length - 1) {
-        wordsThatDoNotIncludeExcludedLetters.push(word);
-      }
+  const wordLetters = word.split(""); // array
+  for (letter of excludedLetters) {
+    // console.log({ letter });
+    if (wordLetters.includes(letter)) {
+      return true;
     }
   }
-
-  if (wordsThatDoNotIncludeExcludedLetters.length !== undefined) {
-    return true;
-  }
-  // do not return more than one datatype
-  // return wordsThatDoNotIncludeExcludedLetters;
+  return false;
 }
-console.log(getWordsThatDoNotIncludeExcludedLetters(), {
-  wordsThatDoNotIncludeExcludedLetters,
-});
+// console.log(isWordThatContainsExcludedLetters("LUB"));
 
-// fn two with main variable globally scoped
-let longestWordThatMeetsExcludedLetterCriteria;
-function getLongestWordsCreatedFromNonuseOfExcludedLetters(arr) {
+function listWordsPerLetterSet(arr) {
+  const specifiedWordsObj = {
+    yessers: [],
+    noers: [],
+  };
+
   for (const word of arr) {
-    if (
-      !longestWordThatMeetsExcludedLetterCriteria ||
-      word.length > longestWordThatMeetsExcludedLetterCriteria.length
-    ) {
-      longestWordThatMeetsExcludedLetterCriteria = word;
+    if (isWordThatContainsExcludedLetters(word)) {
+      specifiedWordsObj.yessers.push(word);
+    } else {
+      specifiedWordsObj.noers.push(word);
     }
   }
-  if (longestWordThatMeetsExcludedLetterCriteria) {
-    return true;
+  return specifiedWordsObj;
+}
+// console.log(listWordsPerLetterSet(wordArr));
+// access specific output instead of all ex below
+// console.log(listWordsPerLetterSet(wordArr).noers);
+// above console log also can be written as below
+// const myObj = listWordsPerLetterSet(wordArr);
+// console.log(myObj.yessers);
+
+function findLongestWordsPerLetterSet() {
+  const longestWordsByLetterSetObj = {};
+
+  let longestYesserWordByLetterSet;
+  let longestNoerWordByLetterSet;
+
+  let allLongestYesserWordsByLetterSet = [];
+  let allLongestNoerWordsByLetterSet = [];
+
+  for (const word of listWordsPerLetterSet(wordArr).yessers) {
+    if (
+      !longestYesserWordByLetterSet ||
+      word.length > longestYesserWordByLetterSet.length
+    ) {
+      longestYesserWordByLetterSet = word;
+      allLongestYesserWordsByLetterSet = [word];
+    } else if (word.length === longestYesserWordByLetterSet.length) {
+      allLongestYesserWordsByLetterSet.push(word);
+    }
+    longestWordsByLetterSetObj.yessers = allLongestYesserWordsByLetterSet;
   }
-  // do not return more than one datatype
-  // return longestWordThatMeetsExcludedLetterCriteria;
-}
-console.log(
-  getLongestWordsCreatedFromNonuseOfExcludedLetters(
-    wordsThatDoNotIncludeExcludedLetters
-  ),
-  { longestWordThatMeetsExcludedLetterCriteria }
-);
 
-let allLongestWordsThatMeetsExcludedLetterCriteria = [];
-for (let i = 0; i < wordsThatDoNotIncludeExcludedLetters.length; i++) {
-  const word = wordsThatDoNotIncludeExcludedLetters[i];
-
-  if (
-    !longestWordThatMeetsExcludedLetterCriteria ||
-    word.length > longestWordThatMeetsExcludedLetterCriteria.length
-  ) {
-    longestWordThatMeetsExcludedLetterCriteria = word;
-    allLongestWordsThatMeetsExcludedLetterCriteria = [word];
-  } else if (
-    word.length === longestWordThatMeetsExcludedLetterCriteria.length
-  ) {
-    allLongestWordsThatMeetsExcludedLetterCriteria.push(word);
+  for (const word of listWordsPerLetterSet(wordArr).noers) {
+    if (
+      !longestNoerWordByLetterSet ||
+      word.length > longestNoerWordByLetterSet.length
+    ) {
+      longestNoerWordByLetterSet = word;
+      allLongestNoerWordsByLetterSet = [word];
+    } else if (word.length === longestNoerWordByLetterSet.length) {
+      allLongestNoerWordsByLetterSet.push(word);
+    }
+    longestWordsByLetterSetObj.noers = allLongestNoerWordsByLetterSet;
   }
+  return longestWordsByLetterSetObj;
 }
-console.log(
-  { longestWordThatMeetsExcludedLetterCriteria },
-  { allLongestWordsThatMeetsExcludedLetterCriteria }
-);
-
-if (
-  getWordsThatDoNotIncludeExcludedLetters(
-    longestWordThatMeetsExcludedLetterCriteria
-  ) === true &&
-  getLongestWordsCreatedFromNonuseOfExcludedLetters(
-    wordsThatDoNotIncludeExcludedLetters
-  ) === true
-) {
-  console.log({ longestWordThatMeetsExcludedLetterCriteria });
-}
+console.log(findLongestWordsPerLetterSet());
