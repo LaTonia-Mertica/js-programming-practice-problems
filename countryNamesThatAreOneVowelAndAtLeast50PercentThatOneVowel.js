103b53b49fd33eb66b0b418172e6f48f2dec0f40
La'Tonia Mertica <latoniamertica@gmail.com>
2022-06-13 12:12:27 -0400

    refactor: countries / one vowel half of its letters

--- countryNamesThatAreOneVowelAndAtLeast50PercentThatOneVowel.js
+++ countryNamesThatAreOneVowelAndAtLeast50PercentThatOneVowel.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const cStr = fs
+const cArr = fs
   .readFileSync("countries.txt")
   .toString("utf-8")
   .toUpperCase()
-  .trim();
-const cArr = cStr.split("\n");
+  .trim()
+  .split("\n");
 
 function getOneVowelCountries(arr) {
   const oneVowelCountries = [];
@@ -16,7 +16,8 @@ function getOneVowelCountries(arr) {
   const invalidChars = [" ", "'", "(", ")", "-", "′", "."];
   let iCount = 0;
 
-  for (const c of cArr) {
+  // initial solution (attempt)
+  for (const c of arr) {
     if (
       c.includes("A") ||
       c.includes("E") ||
@@ -59,7 +60,7 @@ function getOneVowelCountries(arr) {
 
     if (hasOneVowel) {
       oneVowelCountries.push(c);
-      console.log({ oneVowelCountries });
+      // console.log({ oneVowelCountries });
     }
     if (hasOneVowel && atLeast50PercentVowels) {
       oneVowelCountryNamesThatAreAtLeast50PercentTheVowel.push(c);
@@ -67,6 +68,61 @@ function getOneVowelCountries(arr) {
   }
   return oneVowelCountryNamesThatAreAtLeast50PercentTheVowel;
 }
-
 // getOneVowelCountries();
-console.log(getOneVowelCountries());
+// console.log(getOneVowelCountries(cArr));
+
+// START HERE START HERE START HERE START HERE START HERE
+// REFACTOR USING BOOL TO DETERMINE IF VOWEL IS ONLY VOWEL
+function isOneVowelCountry(country) {
+  const vowels = ["A", "E", "I", "O", "U"];
+  let firstV = null;
+  let hasOneV = true;
+  for (const l of country) {
+    if (vowels.includes(l)) {
+      if (!firstV) {
+        firstV = l;
+      } else if (l !== firstV) {
+        hasOneV = false;
+        return false;
+      }
+    }
+  }
+  return true;
+}
+//console.log(isOneVowelCountry("FIJI"));
+
+// REFACTOR USING BOOL TO DETERMINE IF VOWELS AT LEAST HALF OF LETTERS
+function vowelsAtLeastHalfLettersOfCountry(country) {
+  const vowels = ["A", "E", "I", "O", "U"];
+  let vCount = 0;
+  const invalidChars = [" ", "'", "(", ")", "-", "′", "."];
+  let iCount = 0;
+
+  for (const l of country) {
+    if (isOneVowelCountry(country)) {
+      if (vowels.includes(l)) {
+        vCount++;
+      } else if (invalidChars.includes(l)) {
+        iCount++;
+      }
+    }
+  }
+  const trueCountryLength = country.length - iCount;
+  const atLeast50PercentVowels = vCount / trueCountryLength >= 0.5;
+
+  if (atLeast50PercentVowels >= 0.5) {
+    return true;
+  }
+  return false;
+}
+// console.log(vowelsAtLeastHalfLettersOfCountry("CANADA"));
+
+// FN TO OUTPUT SINGLE VOWEL COUNTRIES THAT ARE AT LEAST HALF VOWELS IN LETTERS
+const cWithOneVowelAtLeastHalfLettersOfItsName = [];
+
+for (const country of cArr) {
+  if (vowelsAtLeastHalfLettersOfCountry(country)) {
+    cWithOneVowelAtLeastHalfLettersOfItsName.push(country);
+  }
+}
+console.log({ cWithOneVowelAtLeastHalfLettersOfItsName });
