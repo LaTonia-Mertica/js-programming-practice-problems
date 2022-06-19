dd679d5df38ca416c2b0a6f5e2ed5518ba1dc265
La'Tonia Mertica <latoniamertica@gmail.com>
2022-06-18 21:11:52 -0400

    jess: letter chain / dynamic ranges / longest

--- alphabetChainsWithLettersInAnyOrder.js
+++ alphabetChainsWithLettersInAnyOrder.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
+const wordArr = fs
+  .readFileSync("sowpods.txt")
+  .toString("utf-8")
+  .toUpperCase()
+  .trim()
+  .split("\n");
+
 /*
 QUESTION
 Finding alphabet chains:
-    PART 1
+    PART 1 OF 3
     - First, what are all of the words that have a least one “A”, one “B”, one “C”, one “D”, one “E”, and one “F” in them, in any order?
         - For example, “FEEDBACK” is an answer to this question
 
@@ -11,44 +19,29 @@ Finding alphabet chains:
     - check that each word includes the lettewrs A, B, C, D, E, F (can be in any order)
     - put in a fn
     - console log
-
-    PART 2
-    - Next, is “ABCDEF” the longest alphabet chain that can be found in a word, or is there a longer chain starting somewhere else in the alphabet? Find the longest chain. .. does this mean to check for a letter chain that starts anywhere in alphabet ... would need dynamic start and end to solve ...  
-
-    PSEUDOCODE
-    - 
-
-    PART 3
-    - Try to find longest letter chain in alphabetical order.
-
-
 */
 
-const fs = require("fs");
-const wordArr = fs
-  .readFileSync("sowpods.txt")
-  .toString("utf-8")
-  .toUpperCase()
-  .trim()
-  .split("\n");
+function getWordsContainingLetterChainInAnyOrder() {
+  const wordsContainingLetterChainInAnyOrder = [];
+  for (const word of wordArr) {
+    if (
+      word.includes("A") &&
+      word.includes("B") &&
+      word.includes("C") &&
+      word.includes("D") &&
+      word.includes("E") &&
+      word.includes("F")
+    ) {
+      wordsContainingLetterChainInAnyOrder.push(word);
+    }
+  }
+  return wordsContainingLetterChainInAnyOrder;
+}
+console.log(getWordsContainingLetterChainInAnyOrder());
 
-// function getWordsContainingLetterChainInAnyOrder() {
-//   const wordsContainingLetterChainInAnyOrder = [];
-//   for (const word of wordArr) {
-//     if (
-//       word.includes("A") &&
-//       word.includes("B") &&
-//       word.includes("C") &&
-//       word.includes("D") &&
-//       word.includes("E") &&
-//       word.includes("F")
-//     ) {
-//       wordsContainingLetterChainInAnyOrder.push(word);
-//     }
-//   }
-//   return wordsContainingLetterChainInAnyOrder;
-// }
-// console.log(getWordsContainingLetterChainInAnyOrder());
+/*
+ PART 2 OF 3
+    - Next, is “ABCDEF” the longest alphabet chain that can be found in a word, or is there a longer chain starting somewhere else in the alphabet? Find the longest chain. .. does this mean to check for a letter chain that starts anywhere in alphabet ... need dynamic start and end to solve...  NO, IT IS NOT THE LONGEST LETTER CHAIN */
 
 const letters = [
   "A",
@@ -81,6 +74,7 @@ const letters = [
 const wordsContainingLongerLetterChainInAnyOrder = [];
 let endRangeIdx = 6;
 const baseLetters = letters.slice(0, endRangeIdx);
+// console.log({ baseLetters });
 
 for (const word of wordArr) {
   let isValidWord = true;
@@ -97,9 +91,51 @@ for (const word of wordArr) {
 }
 if (wordsContainingLongerLetterChainInAnyOrder.length === 0) {
   baseLetters.pop();
-  console.log({ baseLetters });
+  // console.log({ baseLetters });
 }
-// console.log({ wordsContainingLongerLetterChainInAnyOrder });
+console.log({ wordsContainingLongerLetterChainInAnyOrder });
+
+/*
+PART 3 OF 3
+    - Try to find longest letter chain in alphabetical order.
+*/
+let result = [];
+let startIdx = 0;
+// let maxLength = letters.length; // 26
+while (endRangeIdx <= letters.length) {
+  // console.log(letters[endRangeIdx]);
+  const wordsWithChain = [];
+
+  const baseLetters = letters.slice(startIdx, endRangeIdx);
+  for (const word of wordArr) {
+    let isValidWord = true;
+
+    for (const letter of baseLetters) {
+      if (!word.includes(letter)) {
+        isValidWord = false;
+      }
+    }
+
+    if (isValidWord) {
+      wordsWithChain.push(word);
+      // console.log({ wordsWithChain });
+    }
+  }
+
+  if (wordsWithChain.length === 0) {
+    baseLetters.pop();
+    // line 127
+    if (baseLetters.length > result.length) {
+      // line 128
+      result = baseLetters;
+    }
+    // console.log({ baseLetters });
+    // break;
+    startIdx++;
+  }
+  endRangeIdx++;
+}
+console.log({ result });
 
 // if get passed line 98 of if statement THEN
 // increment the range index to check each next letter
@@ -107,3 +143,8 @@ if (wordsContainingLongerLetterChainInAnyOrder.length === 0) {
 // and the set of letters previous to that letter make up the longest chain
 
 // - next do a floating end index
+
+// HOME WORK
+// handle ties for max letter chains if others exist
+// do not over think this
+// hint: only lines that need to change are 127, 128
