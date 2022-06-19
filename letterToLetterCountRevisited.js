8b2bd2c92c21e5f310247769d2c8f16f58a7dcbd
La'Tonia Mertica <latoniamertica@gmail.com>
2022-06-18 22:54:35 -0400

    danny: scrabble words solver / indirect js practice

---
+++ letterToLetterCountRevisited.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const WordArr = fs
+  .readFileSync("sowpods.txt")
+  .toString("utf-8")
+  .toUpperCase()
+  .trim()
+  .split("\n");
+
+/*
+  - fn to get count of letter by word
+  - input string
+  - output is obj w/letter as key and value as its occurrence
+  */
+function getLetterCountOfWord(str) {
+  const letterCountObj = {};
+
+  for (const letter of str) {
+    if (!letterCountObj[letter]) {
+      letterCountObj[letter] = 1;
+    } else {
+      letterCountObj[letter]++;
+    }
+  }
+  return letterCountObj;
+}
+console.log(getLetterCountOfWord("PAPLEZ"));
+
+/*
+- fn to check if a certain word can be created from the available letters in the obj
+- input two objs
+----- random scrabble letters
+----- letters of inputted string
+
+- output is a bool
+----- based on if there re enough letters
+
+- return true if can create the word from letters available
+*/
+const scrabbleWordObj = getLetterCountOfWord("PAPLEZ");
+const validWordObj = getLetterCountOfWord("APPLE");
+
+function wordCanBeCreated(scrabbleWordObj, validWordObj) {
+  for (const [key, value] of Object.entries(validWordObj)) {
+    if (!scrabbleWordObj[key] || scrabbleWordObj[key] < value) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(wordCanBeCreated(scrabbleWordObj, validWordObj));
